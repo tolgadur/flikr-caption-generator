448456055b0cbea63d394000a1c715a8ed58d08d
Tolga <mail@tolgadur.com>
2025-02-17 20:43:50 +0000

    fix dataset to return cls token, entire text token sequence

--- src/dataset.py
+++ src/dataset.py
@@ -5,11 +5,14 @@ from datasets import load_dataset
 
 class Flickr30kDataset(torch.utils.data.Dataset):
     def __init__(self, split="train"):
-        dataset = load_dataset("nlphuji/flickr30k", split="test")
+        dataset = load_dataset("nlphuji/flickr30k", split="test").select(range(100))
         self.ds = dataset.filter(lambda x: x["split"] == split)
 
         # load the model and processor to get the image and text embeddings
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.text_model = model.text_model
+        self.vision_model = model.vision_model
+
         self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
     def __len__(self):
@@ -21,11 +24,13 @@ class Flickr30kDataset(torch.utils.data.Dataset):
         captions = self.ds[idx]["caption"]
 
         # get the image and text embeddings
-        inputs = self.processor(
-            images=image, text=captions, return_tensors="pt", padding=True
-        )
-        outputs = self.model(**inputs)
-        image_embeds = outputs.image_embeds
-        text_embeds = outputs.text_embeds
+        image_inputs = self.processor(images=image, return_tensors="pt")
+        text_inputs = self.processor(text=captions, return_tensors="pt", padding=True)
+
+        image_outputs = self.vision_model(**image_inputs)
+        text_outputs = self.text_model(**text_inputs)
+
+        image_embeds = image_outputs.pooler_output  # shape: [1, 768]
+        text_embeds = text_outputs.last_hidden_state  # shape: [5, 22, 512]
 
         return image_embeds, text_embeds
