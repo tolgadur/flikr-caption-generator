cb837ee6ab157d5e71f71e14d239ddd525ef4c07
Tolga <mail@tolgadur.com>
2025-02-20 12:16:50 +0000

    caching error on h100

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,10 @@
 import torch
-from datasets import load_dataset
+from datasets import load_dataset, disable_caching
 
 
 class Flickr30kDataset(torch.utils.data.Dataset):
     def __init__(self, split="train"):
+        disable_caching()  # Disable caching to avoid EXIF issues
         ds = load_dataset("nlphuji/flickr30k", split="test")
         self.ds = ds.filter(lambda x: x["split"] == split)
 
